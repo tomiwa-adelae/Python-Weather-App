75552327228cb2a0bcf27e895f386ce3a2a8c6ad
Tomiwa Adelae <adelaetomiwa6@gmail.com>
2024-10-02 12:46:47 +0100

    Final deployment

---
+++ weather.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+def get_current_weather(city="Lagos"):
+
+    request_url = f"http://api.openweathermap.org/data/2.5/weather?appid={os.getenv('API_KEY')}&q={city}&unit=imperial"
+
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
+if __name__ == '__main__':
+    print('\n*** Get current weather conditions *** \n')
+
+    city = input('\nPlease enter a city name: ')
+
+    # Check for empty strings
+    if not bool(city.strip()):
+        city = 'Lagos'
+
+    weather_data = get_current_weather(city)
+
+    print('\n')
+    pprint(weather_data)
